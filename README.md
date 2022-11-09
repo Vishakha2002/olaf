822f048efbebc4c2e8cb07d72333f2e8d94b13f0
Vishakha Tyagi <vishakhatyagi109@gmail.com>
2022-11-09 07:53:52 -0800

    Refactoring olaf.py Major changes are just converting the main script into smaller functions Also installed Whisper and its requirements Python 3.7.9 is used Updated requirements.txt Updated README.py

--- README.md
+++ README.md
@@ -21,8 +21,51 @@ source ./olaf_dev/bin/activate
 > **_NOTE:_** It is currently work in progress. Hence its not a complete/exhaustive list of packages required by Olaf app.
 ```
 pip3 install streamlit
-pip install streamlit-player
 ```
+<!-- pip install streamlit-player -->
+
+### Install Whisper dependecies
+The codebase also depends on a few Python packages, most notably [HuggingFace Transformers](https://huggingface.co/docs/transformers/index) for their fast tokenizer implementation and [ffmpeg-python](https://github.com/kkroening/ffmpeg-python) for reading audio files. The following command will pull and install the latest commit from this repository, along with its Python dependencies 
+
+    pip install git+https://github.com/openai/whisper.git 
+
+It also requires the command-line tool [`ffmpeg`](https://ffmpeg.org/) to be installed on your system, which is available from most package managers:
+
+```bash
+# on Ubuntu or Debian
+sudo apt update && sudo apt install ffmpeg
+
+# on Arch Linux
+sudo pacman -S ffmpeg
+
+# on MacOS using Homebrew (https://brew.sh/)
+brew install ffmpeg
+
+# on Windows using Chocolatey (https://chocolatey.org/)
+choco install ffmpeg
+
+# on Windows using Scoop (https://scoop.sh/)
+scoop install ffmpeg
+```
+
+You may need [`rust`](http://rust-lang.org) installed as well, in case [tokenizers](https://pypi.org/project/tokenizers/) does not provide a pre-built wheel for your platform. If you see installation errors during the `pip install` command above, please follow the [Getting started page](https://www.rust-lang.org/learn/get-started) to install Rust development environment. Additionally, you may need to configure the `PATH` environment variable, e.g. `export PATH="$HOME/.cargo/bin:$PATH"`. If the installation fails with `No module named 'setuptools_rust'`, you need to install `setuptools_rust`, e.g. by running:
+
+```bash
+pip install setuptools-rust
+```
+
+Python packages required
+```
+pip3 install numpy
+pip3 install torch
+pip3 install tqdm
+pip3 install more-itertools
+pip3 install transformers==4.19.0
+pip3 install 
+ffmpeg-python==0.2.0
+```
+
+
 # Build and install your st_audiorec frontend app
 ```
 cd st_audiorec/frontend
@@ -37,12 +80,9 @@ Once all the packages are installed, we can run the app using following command.
 streamlit run olaf.py
 ```
 
+# Notes
+
 Update  `/Users/vtyagi/code/olaf/olaf_dev/lib/python3.6/site-packages/pafy/backend_youtube_dl.py` look for fixing the youtube download lib issue
 `self._dislikes = 0  #self._ydl_info['dislike_count']`
 
-
-Task I need help with for Frontend:
-- [] Video (Pause/Play) controls can be controlled by keybaord.
-- [] Ask Question buttom can be toggled from keybaord
-- [] Ask Question buttom toggle should start recording the audio as well as pause the youtube video
-- [] Fetch response button should be controlled by keybaord
+If you need to disable bootstrap.css.map error use https://stackoverflow.com/questions/21773376/bootstrap-trying-to-load-map-file-how-to-disable-it-do-i-need-to-do-it
--- olaf.py
+++ olaf.py
@@ -1,191 +1,153 @@
 import os
+import time
 import streamlit.components.v1 as components
 import streamlit as st
 import requests
 from io import BytesIO
 import numpy as np
 
-st.set_page_config(
-    layout="wide",
-)
+# try:
+#     if isinstance(video_event, dict):  # retrieve audio data
+#         if "arr" in video_event.keys():
+#             with st.spinner('Transcribing audio-recording...'):
+#                 placeholder.text('Question recevieved ...')
+#                 ind, val = zip(*video_event['arr'].items())
+#                 ind = np.array(ind, dtype=int)  # convert to np array
+#                 val = np.array(val)             # convert to np array
+#                 sorted_ints = val[ind]
+#                 stream = BytesIO(b"".join([int(v).to_bytes(1, "big") for v in sorted_ints]))
+#                 # This wav_bytes has the audio
+#                 wav_bytes = stream.read()
+#                 print(type(wav_bytes))
 
-# Setup for streamlit_player
-_SUPPORTED_EVENTS = [
-    "onStart", "onPlay", "onProgress", "onDuration", "onPause",
-    "onBuffer", "onBufferEnd", "onSeek", "onEnded", "onError"
-]
-options = {
-    "events": ["onProgress", "onPause"],
-    "progress_interval": 1000
-}
+#                 if 'status' not in st.session_state:
+#                     st.session_state['status'] = 'submitted'
 
-yt_url = "https://www.youtube.com/watch?v=bmIVWe3Cux8&iv_load_policy=3"
+#                 CHUNK_SIZE= 5242880
+#                 upload_endpoint = "https://api.assemblyai.com/v2/upload"
+#                 transcript_endpoint = "https://api.assemblyai.com/v2/transcript"
 
-av_player_parent_dir = os.path.dirname(os.path.abspath(__file__))
-av_player_build_dir = os.path.join(av_player_parent_dir, "av_player/frontend/build")
-av_player = components.declare_component("streamlit_player", path=av_player_build_dir)
+#                 headers = {
+#                     "authorization":"34e0f41f2b84459db61bb5da43686224",
+#                     "contect-type": "application/json"
+#                 }
 
-# STREAMLIT Video Player Instance
-video_event = av_player(url=yt_url, width ="800px", height = "400px", **options)
-if isinstance(video_event, dict):
-    try:
-        if video_event.get('name') == "onPause":
-            print("video got paused")
-        if video_event.get('name') == "onProgress":
-            if video_event.get('data') and type(video_event.get('data') == dict):
-                frameStoppedAt = video_event["data"].get("playedSeconds")
-                st.text(f"you paused the video at {frameStoppedAt}")
-    except KeyError:
-        print("\n")
+#                 def read_file(filename):
+#                     with open(filename, 'rb') as _file:
+#                         while True:
+#                             data = _file.read(CHUNK_SIZE)
+#                             if not data:
+#                                 break
+#                             yield data
 
-placeholder = st.empty()
-try:
+#                 def start_transcription():
+#                     # upload_response = requests.post(
+#                     #     upload_endpoint,
+#                     #     headers=headers, data=read_file(audio_file)
+#                     # )
+#                     upload_response = requests.post(
+#                         upload_endpoint,
+#                         headers=headers, data=wav_bytes
+#                     )
+#                     audio_url = upload_response.json()['upload_url']
+#                     print(f"Uploaded to {audio_url}")
+
+#                     transcript_request = {
+#                         'audio_url': audio_url,
+#                         'iab_categories': 'False',
+#                     }
+#                     transcript_response = requests.post(transcript_endpoint, json=transcript_request, headers=headers)
+
+#                     transcript_id = transcript_response.json()['id']
+#                     polling_endpoint = transcript_endpoint + "/" + transcript_id
+#                     return polling_endpoint
+
+#                 polling_endpoint = start_transcription()
+#                 transcript = None
+#                 while st.session_state['status'] != 'completed':
+#                     polling_response = requests.get(polling_endpoint, headers=headers)
+#                     st.session_state['status'] = polling_response.json()['status']
+#                     # print(f"polling status {polling_response.json()['status']}")
+#                     transcript = polling_response.json()['text']
+
+#                 placeholder.text('Your Question -')
+#                 st.markdown(transcript)
+#                 st.session_state['status'] = "submitted"
+#                 # transcript = "This is a test"
+#                 # placeholder.text('Your Question -')
+#                 # st.markdown(transcript)
+#                 # st.session_state['status'] = "submitted"
+
+# except KeyError:
+#     print("\n")
+
+def transcribe_question(video_event, default="assemblyai"):
+    if default == "assemblyai":
+        time.sleep(3)
+        return "Not Implemented"
+    
+
+def parse_video_event(video_event):
+    # print(st.session_state.is_video_paused)
+    if video_event and video_event.get('name') == "onPause":
+            st.session_state.is_video_paused = True 
+            # print("video got paused")
+    if video_event and video_event.get('name') == "onProgress":
+        # print("Inside onProgres")
+        if video_event.get('data') and isinstance(video_event.get('data'), dict):
+            frame_stopped_at = video_event["data"].get("playedSeconds")
+            # print(f"you paused the video at {frame_stopped_at} session state {st.session_state.is_video_paused}")
+            if st.session_state.is_video_paused:
+                print(f"you paused the video at {frame_stopped_at}")
+                st.session_state.frame_stopped_at = frame_stopped_at
+                st.session_state.is_video_paused = False
+                st.write(st.session_state)
+
+
+def initialize_session_state():
+    if "is_video_paused" not in st.session_state:
+        st.session_state['is_video_paused'] = False
+    if "frame_stopped_at" not in st.session_state:   
+        st.session_state['frame_stopped_at'] = 0
+
+
+def main():
+    initialize_session_state()
+
+    st.set_page_config(
+        layout="wide",
+    )
+
+    # Setup for streamlit_player
+    _SUPPORTED_EVENTS = [
+        "onStart", "onPlay", "onProgress", "onDuration", "onPause",
+        "onBuffer", "onBufferEnd", "onSeek", "onEnded", "onError"
+    ]
+    options = {
+        # "events": ["onProgress", "onPause"],
+        "events": _SUPPORTED_EVENTS,
+        "progress_interval": 1000
+    }
+
+    # XXX Take user input instead of harcoding.
+    # yt_url = "https://www.youtube.com/watch?v=bmIVWe3Cux8&iv_load_policy=3"
+    yt_url = "https://www.youtube.com/watch?v=nK1r_9hPWuI"
+
+    av_player_parent_dir = os.path.dirname(os.path.abspath(__file__))
+    av_player_build_dir = os.path.join(av_player_parent_dir, "av_player/frontend/build")
+    av_player = components.declare_component("streamlit_player", path=av_player_build_dir)
+
+    # STREAMLIT Video Player Instance
+    video_event = av_player(url=yt_url, width ="500px", height = "300px", **options)
+
+    parse_video_event(video_event)
+    placeholder = st.empty()
     if isinstance(video_event, dict):  # retrieve audio data
         if "arr" in video_event.keys():
-            with st.spinner('Transcribing audio-recording...'):
-                placeholder.text('Question recevieved ...')
-                ind, val = zip(*video_event['arr'].items())
-                ind = np.array(ind, dtype=int)  # convert to np array
-                val = np.array(val)             # convert to np array
-                sorted_ints = val[ind]
-                stream = BytesIO(b"".join([int(v).to_bytes(1, "big") for v in sorted_ints]))
-                wav_bytes = stream.read()
-                print(type(wav_bytes))
-
-                if 'status' not in st.session_state:
-                    st.session_state['status'] = 'submitted'
-
-                CHUNK_SIZE= 5242880
-                upload_endpoint = "https://api.assemblyai.com/v2/upload"
-                transcript_endpoint = "https://api.assemblyai.com/v2/transcript"
-
-                headers = {
-                    "authorization":"34e0f41f2b84459db61bb5da43686224",
-                    "contect-type": "application/json"
-                }
-
-                def read_file(filename):
-                    with open(filename, 'rb') as _file:
-                        while True:
-                            data = _file.read(CHUNK_SIZE)
-                            if not data:
-                                break
-                            yield data
-
-                def start_transcription():
-                    # upload_response = requests.post(
-                    #     upload_endpoint,
-                    #     headers=headers, data=read_file(audio_file)
-                    # )
-                    upload_response = requests.post(
-                        upload_endpoint,
-                        headers=headers, data=wav_bytes
-                    )
-                    audio_url = upload_response.json()['upload_url']
-                    print(f"Uploaded to {audio_url}")
-
-                    transcript_request = {
-                        'audio_url': audio_url,
-                        'iab_categories': 'False',
-                    }
-                    transcript_response = requests.post(transcript_endpoint, json=transcript_request, headers=headers)
-
-                    transcript_id = transcript_response.json()['id']
-                    polling_endpoint = transcript_endpoint + "/" + transcript_id
-                    return polling_endpoint
-
-                # polling_endpoint = start_transcription()
-                transcript = None
-                # while st.session_state['status'] != 'completed':
-                #     polling_response = requests.get(polling_endpoint, headers=headers)
-                #     st.session_state['status'] = polling_response.json()['status']
-                #     print(f"polling status {polling_response.json()['status']}")
-                #     transcript = polling_response.json()['text']
-
-                # placeholder.text('Your Question -')
-                # st.markdown(transcript)
-                # st.session_state['status'] = "submitted"
-                transcript = "This is a test"
-                placeholder.text('Your Question -')
-                st.markdown(transcript)
-                st.session_state['status'] = "submitted"
-
-except KeyError:
-    print("\n")
+            with st.spinner('Transcribing Question...'):
+                transcription = transcribe_question(video_event)
+                placeholder.text(transcription)
 
 
-# Modified version from https://github.com/stefanrmmr/streamlit_audio_recorder
-# Setup for st_audio
-# st_audio_parent_dir = os.path.dirname(os.path.abspath(__file__))
-# st_audio_build_dir = os.path.join(st_audio_parent_dir, "st_audiorec/frontend/build")
-# st_audiorec = components.declare_component("st_audiorec", path=st_audio_build_dir)
-
-# STREAMLIT AUDIO RECORDER Instance
-# val = st_audiorec()
-
-# placeholder = st.empty()
-# if isinstance(val, dict):  # retrieve audio data
-#     placeholder.text('Question recevieved ...')
-#     with st.spinner('Transcribing audio-recording...'):
-# #         ind, val = zip(*val['arr'].items())
-# #         ind = np.array(ind, dtype=int)  # convert to np array
-# #         val = np.array(val)             # convert to np array
-# #         sorted_ints = val[ind]
-# #         stream = BytesIO(b"".join([int(v).to_bytes(1, "big") for v in sorted_ints]))
-# #         wav_bytes = stream.read()
-# # #         print(type(wav_bytes))
-
-# #         if 'status' not in st.session_state:
-# #             st.session_state['status'] = 'submitted'
-
-# #         CHUNK_SIZE= 5242880
-# #         audio_file="/Users/vtyagi/Desktop/sky_is_blue.wav"
-# #         upload_endpoint = "https://api.assemblyai.com/v2/upload"
-# #         transcript_endpoint = "https://api.assemblyai.com/v2/transcript"
-
-# #         headers = {
-# #             "authorization":"34e0f41f2b84459db61bb5da43686224",
-# #             "contect-type": "application/json"
-# #         }
-
-# #         def read_file(filename):
-# #             with open(filename, 'rb') as _file:
-# #                 while True:
-# #                     data = _file.read(CHUNK_SIZE)
-# #                     if not data:
-# #                         break
-# #                     yield data
-
-# #         def start_transcription():
-# #             # upload_response = requests.post(
-# #             #     upload_endpoint,
-# #             #     headers=headers, data=read_file(audio_file)
-# #             # )
-# #             upload_response = requests.post(
-# #                 upload_endpoint,
-# #                 headers=headers, data=wav_bytes
-# #             )
-# #             audio_url = upload_response.json()['upload_url']
-# #             print(f"Uploaded to {audio_url}")
-
-# #             transcript_request = {
-# #                 'audio_url': audio_url,
-# #                 'iab_categories': 'False',
-# #             }
-# #             transcript_response = requests.post(transcript_endpoint, json=transcript_request, headers=headers)
-
-# #             transcript_id = transcript_response.json()['id']
-# #             polling_endpoint = transcript_endpoint + "/" + transcript_id
-# #             return polling_endpoint
-
-# #         polling_endpoint = start_transcription()
-# #         transcript = None
-# #         while st.session_state['status'] != 'completed':
-# #             polling_response = requests.get(polling_endpoint, headers=headers)
-# #             st.session_state['status'] = polling_response.json()['status']
-# #             print(f"polling status {polling_response.json()['status']}")
-# #             transcript = polling_response.json()['text']
-        # transcript = "This is a test"
-        # placeholder.text('Your Question -')
-        # st.markdown(transcript)
-        # st.session_state['status'] = "submitted"
+if __name__ == "__main__":
+    main()
